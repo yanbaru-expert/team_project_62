cf9a941c0322910067d5c19731b78e6fcc8beb3a
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-04-17 17:51:20 +0900

    rubocopで修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-email = 'test@example.com'
-password = 'password'
+email = "test@example.com"
+password = "password"
 
 User.find_or_create_by!(email: email) do |user|
-    user.password = password
-    puts 'ユーザーの初期データインポートに成功しました。'
-  end
+  user.password = password
+  puts "ユーザーの初期データインポートに成功しました。"
+end
