87df30dcbc622d854ee3fecf0a16432e9b5fce38
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-04-26 19:25:39 +0900

    rubocop実施

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,7 @@ module TeamProject
     config.load_defaults 6.1
 
     config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
+    config.time_zone = "Asia/Tokyo"
 
     # Configuration for the application, engines, and railties goes here.
     #
