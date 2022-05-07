ae2e8eeeb71c2e4a18b8b11eec574a84cf990193
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-07 09:45:51 +0900

    seeds.rbのAdminUser部分の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ require "import_csv"
 end
 
 email = "test@example.com"
+admin_email = "admin@example.com"
 password = "password"
 
 User.find_or_create_by!(email: email) do |user|
@@ -13,7 +14,11 @@ User.find_or_create_by!(email: email) do |user|
   puts "ユーザーの初期データインポートに成功しました。"
 end
 
+AdminUser.find_or_create_by!(email: admin_email) do |admin_user|
+  admin_user.password = password
+  puts "アドミンユーザの初期データインポートに成功しました。"
+end
+
 ImportCsv.import_text_data("db/csv_data/text_data.csv")
 ImportCsv.import_movie_data("db/csv_data/movie_data.csv")
 puts "CSVファイルのインポートに成功しました。"
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
