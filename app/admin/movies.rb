40dff7e96143ad342a6eec1752f6ea0f0d17c924
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-07 11:09:27 +0900

    管理画面の各ページのカラム・フォームを修正

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,16 +1,32 @@
 ActiveAdmin.register Movie do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :genre, :title, :url
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:genre, :title, :url]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :genre, :title, :url
+
+  index do
+    selectable_column
+    id_column
+    # enum-help を利用
+    column :genre, :movie, &:genre_i18n
+    column :title
+    column :url
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      # enum-help を利用
+      input :genre, as: :select, collection: Movie.genres_i18n.invert
+      input :title
+      input :url
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Movie.genres_i18n.invert.transform_values { |v| Movie.genres[v] }
+  filter :title
+  filter :url
+  filter :created_at
+  filter :updated_at
 end
