fb34ab9fd26441be89cd6d2d9f15eacf42f924e7
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-05-12 20:18:43 +0900

    一覧表示の実装中

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,13 +1,13 @@
 class TextsController < ApplicationController
   
   def index
-    
+
     if params[:genre] == nil
-      @tests = Text.where(genre:[%w[basic git ruby rails]])
+      @tests = Text.where(genre:[basic git ruby rails])
+      binding.pry
     else
       @tests = Text.where(genre:params[:genre])
     end
-   
   end
 
   def show; end
