1a164044ace7516c50aae0a7cf0ebd5e5ebe2e0b
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-05-13 03:33:32 +0900

    Railsテキスト教材一覧ページの実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,12 +1,10 @@
 class TextsController < ApplicationController
-  
   def index
-
-    if params[:genre] == nil
-      @texts = Text.where(genre: ["basic", "git", "ruby", "rails"])
-    else
-      @texts = Text.where(genre: params[:genre])
-    end
+    @texts = if params[:genre].nil?
+               Text.where(genre: ["basic", "git", "ruby", "rails"])
+             else
+               Text.where(genre: params[:genre])
+             end
   end
 
   def show; end
