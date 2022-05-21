4312760a0d34d9897dd4271226baf9f279e8fb46
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-05-21 17:56:01 +0900

    rubocop 実施

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
-    def guest_sign_in
-      # ゲストアカウントでログイン
-      sign_in User.guest
-      # トップページへリダイレクト
-      redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
-    end
+  def guest_sign_in
+    sign_in User.guest
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
   end
+end
