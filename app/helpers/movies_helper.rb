e31c3535eee55e1eb76b381ab98fa217a924129b
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-22 09:45:29 +0900

    動画教材タイトルを切り替える処理を記述

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,4 +1,7 @@
 module MoviesHelper
+  PHP_MOVIES_TITLE = "PHP動画教材".freeze
+  RAILS_MOVIES_TITLE = "Ruby/Rails 動画".freeze
+
   def embed_youtube(url)
     tag.iframe(
       width: "100%",
@@ -8,5 +11,10 @@ module MoviesHelper
       allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
       allowfullscreen: true
     )
+  end
+
+  # ===== 動画教材のタイトルを切り替える処理 =====
+  def switch_movies_title
+    params[:genre] == "php" ? PHP_MOVIES_TITLE : RAILS_MOVIES_TITLE
   end
 end
