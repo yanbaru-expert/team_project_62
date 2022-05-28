b8c3b7c73690b67e8a8f4173282e7786b7474ac8
iloveomelette <mizuki1998hosei@gmail.com>
2022-05-28 17:00:30 +0900

    動画教材ページにページネーションを実装しレベル表示の修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   RAILS_GENRE_LIST = %w[basic git ruby rails].freeze
+  PER_PAGE = 12
 
   with_options presence: true do
     validates :genre
@@ -19,9 +20,9 @@ class Movie < ApplicationRecord
   # ===== Rails動画教材かPHP動画教材のどちらを表示するかの処理 =====
   def self.switch_movies_by_genres(params)
     if params[:genre] == "php"
-      Movie.where(genre: "php").order(id: :asc)
+      Movie.where(genre: "php").order(id: :asc).page(params[:page]).per(PER_PAGE)
     else
-      Movie.where(genre: Movie::RAILS_GENRE_LIST).order(id: :asc)
+      Movie.where(genre: Movie::RAILS_GENRE_LIST).order(id: :asc).page(params[:page]).per(PER_PAGE)
     end
   end
 end
