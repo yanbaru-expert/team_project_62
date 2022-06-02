405b2cc9dbd135e15f5b8384a5c2af44160abf45
iloveomelette <mizuki1998hosei@gmail.com>
2022-06-02 22:52:31 +0900

    switch_movies_by_genresの修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,7 @@
 class MoviesController < ApplicationController
-  RAILS_GENRE_LIST = %w[basic git ruby rails].freeze
   PER_PAGE = 12
 
   def index
-    @movies = params[:genre] == "php" ? switch_movies_by_genres("php") : switch_movies_by_genres(RAILS_GENRE_LIST)
-  end
-
-  private
-
-  # ===== Rails動画教材かPHP動画教材のどちらを表示するかの処理 =====
-  def switch_movies_by_genres(genre)
-    Movie.where(genre: genre).order(id: :asc).page(params[:page]).per(PER_PAGE)
+    @movies = Movie.switch_movies_by_genres(params).page(params[:page]).per(PER_PAGE)
   end
 end
