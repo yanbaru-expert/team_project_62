2c0d798343b02841fe74aaf4ebcda22f036e806d
iloveomelette <mizuki1998hosei@gmail.com>
2022-06-02 09:58:45 +0900

    動画教材をジャンル別に表示する処理をコントローラに移行

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,4 @@
 class Movie < ApplicationRecord
-  RAILS_GENRE_LIST = %w[basic git ruby rails].freeze
-  PER_PAGE = 12
-
   with_options presence: true do
     validates :genre
     validates :title
@@ -16,13 +13,4 @@ class Movie < ApplicationRecord
     rails: 4,
     php: 5
   }
-
-  # ===== Rails動画教材かPHP動画教材のどちらを表示するかの処理 =====
-  def self.switch_movies_by_genres(params)
-    if params[:genre] == "php"
-      Movie.where(genre: "php").order(id: :asc).page(params[:page]).per(PER_PAGE)
-    else
-      Movie.where(genre: Movie::RAILS_GENRE_LIST).order(id: :asc).page(params[:page]).per(PER_PAGE)
-    end
-  end
 end
