467f0cdb1af16601209b21f1a5dc4bef967bfc9f
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-06-02 20:52:46 +0900

    moview_helperをpagetitle_helperhへ変更

--- app/helpers/texts_helper.rb
+++
@@ -1,7 +0,0 @@
-module TextsHelper
-  PHP_TEXTS_TITLE = "PHP".freeze
-  RAILS_TEXTS_TITLE = "Ruby/Rails".freeze
-  def select_pagetitle
-    params[:genre] == "php" ? PHP_TEXTS_TITLE : RAILS_TEXTS_TITLE
-  end
-end
