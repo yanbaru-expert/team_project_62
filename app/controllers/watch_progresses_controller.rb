0b89347709489b66ffe083931612b0805d24a094
iloveomelette <mizuki1998hosei@gmail.com>
2022-06-07 15:41:14 +0900

    WatchProgressの非同期化

--- app/controllers/watch_progresses_controller.rb
+++ app/controllers/watch_progresses_controller.rb
@@ -1,11 +1,17 @@
 class WatchProgressesController < ApplicationController
+  before_action :set_movie, only: %i[create destroy]
+
   def create
-    current_user.watch_progresses.create!(movie_id: params[:movie_id])
-    redirect_back(fallback_location: root_path)
+    current_user.watch_progresses.create!(movie_id: @movie.id)
   end
 
   def destroy
-    current_user.watch_progresses.destroy!(movie_id: params[:movie_id])
-    redirect_back(fallback_location: root_path)
+    current_user.watch_progresses.find_by(movie_id: @movie.id).destroy!
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
