e77d453a6340be61c1c24d8f155b93f1bb16855e
iloveomelette <mizuki1998hosei@gmail.com>
2022-06-07 14:14:00 +0900

    WatchProgress#create,destroyの定義とビュー表示の記述

--- app/controllers/watch_progresses_controller.rb
+++ app/controllers/watch_progresses_controller.rb
@@ -1,2 +1,11 @@
 class WatchProgressesController < ApplicationController
+  def create
+    current_user.watch_progresses.create!(movie_id: params[:movie_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.watch_progresses.destroy!(movie_id: params[:movie_id])
+    redirect_back(fallback_location: root_path)
+  end
 end
