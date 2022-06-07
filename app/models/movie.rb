d652913444f13872387194748e0f37ac69076fe0
iloveomelette <mizuki1998hosei@gmail.com>
2022-06-07 14:30:30 +0900

    WatchProgressの部分テンプレート化とN+1問題の解消

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,13 +21,13 @@ class Movie < ApplicationRecord
   # ===== Rails動画教材かPHP動画教材のどちらを表示するかの処理 =====
   def self.switch_movies_by_genres(params)
     if params[:genre] == "php"
-      Movie.where(genre: "php").order(id: :asc)
+      Movie.includes(:watch_progresses).where(genre: "php").order(id: :asc)
     else
-      Movie.where(genre: RAILS_GENRE_LIST).order(id: :asc)
+      Movie.includes(:watch_progresses).where(genre: RAILS_GENRE_LIST).order(id: :asc)
     end
   end
 
   def watched_or_not?(user)
-    watch_progresses.exists?(user_id: user.id)
+    watch_progresses.any? { |watch_progress| watch_progress.user_id == user.id }
   end
 end
